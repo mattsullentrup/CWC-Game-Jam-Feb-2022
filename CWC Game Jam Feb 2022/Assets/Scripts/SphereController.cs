b12b0fc30adec84d0b0234d0ebc29582ddb406d8
mattsullentrup <msullentrup1@gmail.com>
2022-02-18 18:48:55 -0600

    Player movement/camera follow

--- CWC Game Jam Feb 2022/Assets/Scripts/SphereController.cs
+++ CWC Game Jam Feb 2022/Assets/Scripts/SphereController.cs
@@ -6,10 +6,13 @@ public class SphereController : MonoBehaviour
 {
     public float playerSpeed = 10f;
     private Rigidbody playerRb;
+    private Camera cam;
+    
     // Start is called before the first frame update
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        cam = Camera.main;
     }
 
     // Update is called once per frame
@@ -24,7 +27,33 @@ public class SphereController : MonoBehaviour
         float horizontalInput = Input.GetAxis("Horizontal");
         float verticalInput = Input.GetAxis("Vertical");
 
-        Vector3 movement = playerSpeed * Time.fixedDeltaTime * new Vector3(horizontalInput, 0, verticalInput);
-        playerRb.AddForce(movement);
+        //Vector3 movement = new Vector3(horizontalInput, 0, verticalInput).normalized;
+        //playerRb.AddForce(playerSpeed * Time.fixedDeltaTime * movement, ForceMode.Acceleration);
+
+        // Get directions relative to camera
+        Vector3 forward = cam.transform.forward;
+        Vector3 right = cam.transform.right;
+
+        // Project forward and right direction on the horizontal plane (not up and down), then
+        // normalize to get magnitude of 1
+        forward.y = 0;
+        right.y = 0;
+        forward.Normalize();
+        right.Normalize();
+
+        // Set the direction for the player to move
+        Vector3 dir = right * horizontalInput + forward * verticalInput;
+
+        // Set the direction's magnitude to 1 so that it does not interfere with the movement speed
+        dir.Normalize();
+
+        // Move the player by the direction multiplied by speed and delta time 
+        playerRb.AddForce(playerSpeed * Time.deltaTime * dir);
+
+        // Set rotation to direction of movement if moving
+        //if (dir != Vector3.zero)
+        //{
+        //    transform.rotation = Quaternion.Slerp(transform.rotation, Quaternion.LookRotation(forward), 0.2f);
+        //}
     }
 }
